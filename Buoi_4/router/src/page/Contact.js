fcadc4531f3e1bb640b78d901dbfaeae6f3a80be
Hảo Nguyễn <nguyenphuochao123@gmail.com>
2024-02-07 01:19:00 +0700

    m

---
+++ Buoi_4/router/src/page/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <>
+            Đây là trang contact
+        </>
+    )
+}
+
+export default Contact
---
+++ Buoi_4/router/src/page/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            Đây là trang home
+        </>
+    )
+}
+
+export default Home
